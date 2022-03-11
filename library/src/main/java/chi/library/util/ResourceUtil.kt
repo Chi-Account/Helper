466725421312dd5048b58f7ec6a8a8c1dcc6fbcd
张智琦 <chi_account@163.com>
2022-03-11 09:10:37 +0800

    优化 ResourceUtil

--- library/src/main/java/chi/library/util/ResourceUtil.kt
+++ library/src/main/java/chi/library/util/ResourceUtil.kt
@@ -1,12 +1,12 @@
 package chi.library.util
 
+import android.content.Context
 import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
-import chi.library.base.BaseApplication
 
-fun getResourceColor(@ColorRes id: Int) =
-    ContextCompat.getColor(BaseApplication.context, id)
+fun Context.getResourceColor(@ColorRes id: Int) =
+    ContextCompat.getColor(applicationContext, id)
 
-fun getResourceString(@StringRes resId: Int) =
-    BaseApplication.context.getString(resId)
+fun Context.getResourceString(@StringRes resId: Int) =
+    applicationContext.getString(resId)
